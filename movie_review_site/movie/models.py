167c8e354d97f6ac25221ceaca117fbfd9a6c96d
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-31 15:08:24 +0300

    adding Movie model

--- movie_review_site/movie/models.py
+++ movie_review_site/movie/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Movie(models.Model):
+  title = models.CharField(max_length=100)
+  date = models.DateField(auto_now_add=True)
+  author = models.CharField(max_length=30)
+  description = models.TextField()
+  image = models.ImageField(upload_to='movie/images')
+  trailers = models.TextField(null=True)
+  
+def __str__(self):
+  return self.text
