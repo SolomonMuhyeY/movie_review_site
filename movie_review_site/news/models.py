bf6ed12ec844ffff44bfd94356ccc91753df0142
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-31 13:27:44 +0300

    working on the news section

--- movie_review_site/news/models.py
+++ movie_review_site/news/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class News(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.CharField(max_length=30)
+  description = models.TextField()
+  title = models.CharField(max_length=100)
+  date = models.DateField(auto_now_add=True, editable=True)
+  image = models.ImageField(upload_to='news/image')
+  
+def __str__(self):
+  return self.text
+    
--- movie_review_site/news/views.py
+++ movie_review_site/news/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from .models import News
 
 # Create your views here.
+def news(request):
+  newss = News.objects.all()
+  return render(request,'news/news.html',{'newss':newss})
