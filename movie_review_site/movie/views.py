298b9812a7f4f94b0e6b5d7add094ec413600da6
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-31 15:09:23 +0300

    adding home handler view

--- movie_review_site/movie/views.py
+++ movie_review_site/movie/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
+def home(request):
+  movies = Movie.objects.all()
+  return render(request,'movie/home.html',{'movies':movies})
