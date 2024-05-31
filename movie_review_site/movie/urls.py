7f338be4d4b22208c43d1bdbffb4b8ace3dfeced
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-31 16:22:21 +0300

    adding detail handler in view.py

--- movie_review_site/movie/urls.py
+++ movie_review_site/movie/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('',views.home,name='home')
+  path('',views.home,name='home'),
+  path('<int:movie_id>',views.detail,name='detail')
 ]
