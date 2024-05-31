28249ca94ad07eea45edb867f8123ce6ea451c56
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-05-31 16:22:55 +0300

    final commit for the day

--- movie_review_site/movie/views.py
+++ movie_review_site/movie/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Movie
 # Create your views here.
 def home(request):
   movies = Movie.objects.all()
   return render(request,'movie/home.html',{'movies':movies})
+def detail(request,movie_id):
+  movie = get_object_or_404(Movie,pk=movie_id)
+  return render(request,'movie/detail.html',{'movie':movie})
