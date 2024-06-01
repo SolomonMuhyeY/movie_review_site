2af5100ae6dde78040521050511ab967ff7ce401
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-01 17:44:28 +0300

    adding Review model

--- movie_review_site/movie/models.py
+++ movie_review_site/movie/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.forms import ModelForm
+from django.contrib.auth.models import User
 # Create your models here.
 class Movie(models.Model):
   title = models.CharField(max_length=100)
@@ -10,4 +11,13 @@ class Movie(models.Model):
   trailers = models.TextField(null=True)
   
 def __str__(self):
-  return self.text
+  return self.title
+
+
+class Review(models.Model):
+  review_text = models.TextField()
+  date = models.DateTimeField(auto_now_add=True, editable=True)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  movie = models.ForeignKey(Movie,on_delete=models.CASCADE)
+  def __str__(self):
+    return self.review_text[:50]
