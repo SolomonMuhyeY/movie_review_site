aae51f4317037ad53fb938a12c5d3067c250755a
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-06 13:42:30 +0300

    adding user profile model

--- movie_review_site/accounts/models.py
+++ movie_review_site/accounts/models.py
@@ -1,3 +1,12 @@
+# models.py
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='profile/image/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+    
+    
