7e09d095f621b77ace09887f45164c3c580aa25e
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-06 13:43:15 +0300

    adding profile to the url pattter

--- movie_review_site/accounts/urls.py
+++ movie_review_site/accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
   path('signup/',views.signup, name='signup'),
   path('login/',views.loginaccount, name='login'),
   path('logout/',views.logoutaccount, name='logout'),
+  path('profile/', views.update_profile_image, name='update_profile_image'),
 ]
