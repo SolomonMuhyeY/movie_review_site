617c8d04ece56eee75b54ed4b78d917ae421cf9b
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-08 01:03:08 +0300

    adding VoteForm

--- movie_review_site/movie/forms.py
+++ movie_review_site/movie/forms.py
@@ -1,13 +1,20 @@
-from django import forms
+from django.forms import ModelForm,Textarea
 from .models import Review
+from django import forms
+from .models import Vote
 
-class ReviewForm(forms.ModelForm):
+class ReviewForm(ModelForm):
     class Meta:
         model = Review
         fields = ['review_text']
         widgets = {
-            'review_text': forms.Textarea(attrs={
+            'review_text': Textarea(attrs={
                 'rows': 2,
                 'class': 'form-input mt-1 block w-full md:w-64 rounded-md shadow-sm border-lime-300 focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50 bg-gray-800 text-gray-100'
             })
         }
+
+class VoteForm(forms.ModelForm):
+    class Meta:
+        model = Vote
+        fields = ['vote_type']
