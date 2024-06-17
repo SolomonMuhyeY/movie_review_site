60e846ede8f55a8bc362e4410ae5498765e6a358
SolomonMuhye <solomon.muhye.wd@gmail.com>
2024-06-17 14:00:32 +0300

    adding user profile edit section

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
   path('signup/',views.signup, name='signup'),
   path('login/',views.loginaccount, name='login'),
   path('logout/',views.logoutaccount, name='logout'),
-  path('profile/', views.update_profile_image, name='update_profile_image'),
+  path('profile/',views.profile, name='profile'),
+  path('edit_profile/', views.update_profile_image, name='update_profile_image'),
 ]
